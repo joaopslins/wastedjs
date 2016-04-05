9528731d09c1527235163554d9951d3e1799ff85
Joao Paulo Siqueira Lins <jpsl@ug2c43.windows.cin.ufpe.br>
2016-04-05 16:54:15 -0300

    Main page WIP Part 1

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,27 @@
+var fodinhaJS = angular.module ("fodinhaJSapp", ["ngAnimate"]);
+
+fodinhaJS.controller("fodinhaJSctrl", function($scope)
+{
+	$scope.loggedIn = false;
+	$scope.nick = "";
+
+	$scope.loginToggle = function()
+	{
+		// If logging in
+		if(!$scope.loggedIn)
+		{
+
+		}
+		// If logging out
+		else
+		{
+
+		}
+
+		$scope.loggedIn = !$scope.loggedIn;
+	}
+});
+
 $(function() {
 	var $button = $("#button");
 
