1b9bbe40c928a2f468f964a983ae3fdefd12c88b
Joao Paulo Siqueira Lins <jpsl@uhwc06.windows.cin.ufpe.br>
2016-04-13 11:41:07 -0300

    TODO: Players logic and test separate js files

---
+++ game/player.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var Player = function () {
+    this.lives = Player.prototype.LIVES;
+
+    this.hand = new Deck();
+    this.chosen = false;
+}
+
+Player.prototype.LIVES = 3;
+
+Player.prototype.takeCard = function(card) {
+    this.hand.push(card);
+};
+
+Player.prototype.play = function() {
+    var card = this.hand.remove(this.chosen);
+    this.chosen = false;
+
+    return card;
+};
+
+Player.prototype.choose = function(choice) {
+    this.chosen = choice;
+};
+
+Player.prototype.toString = function() {
+    return ''+ this.hand;
+};
+
+module.exports = Player;
--- public/js/main.js
+++ public/js/main.js
@@ -4,27 +4,56 @@ fodinhaJS.controller("fodinhaJSctrl", function($scope, $timeout)
 {
 	$scope.loggedIn = 'no';
 	$scope.nick = "";
+	$scope.ready = false;
 
 	$scope.loginToggle = function()
 	{
 		// If logging in
 		if($scope.loggedIn == "no")
 		{
+			// Animation
 			$scope.loggedIn = false;
 			$timeout( function() {
 				$scope.loggedIn = "yes";
 			}, 500);
+
+			//Add new player TODO - SERVER
+			$scope.players.push(
+				{
+					name : $scope.nick,
+					ready : $scope.ready
+				}
+			);
+
 		}
 		// If logging out
 		else if($scope.loggedIn == "yes")
 		{
+			// Animation
 			$scope.loggedIn = false;
 			$timeout( function() {
 				$scope.loggedIn = "no";
 			}, 500);
+
+			//Remove current player TODO - SERVER
+			$scope.players = $scope.players.filter(function (player)
+			{
+				return player.name != $scope.nick;
+			});
 		}
 
 	}
+
+	$scope.players = [
+	{
+		name : "Teste1",
+		ready : false
+	},
+	{
+		name : "Teste2",
+		ready : false
+	}
+	];
 });
 
 // $(function() {
