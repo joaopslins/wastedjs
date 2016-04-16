90ae3bbdf1faebcc6f458155eb0f356f0e8d37d0
Jota914 <jpsl@cin.ufpe.br>
2016-04-16 11:36:37 -0300

    game table and player objects

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,18 @@
 var fodinhaJS = angular.module ("fodinhaJSapp", ["ngAnimate"]);
 
+// TODO - CARDS
+
 fodinhaJS.controller("fodinhaJSctrl", function($scope, $timeout)
 {
 	$scope.loggedIn = 'no';
-	$scope.nick = "";
-	$scope.ready = false;
+	$scope.me = {
+		name : "",
+		ready : false,
+		lives : 1,
+		won : 2,
+		bet : 0
+
+	}
 
 	$scope.loginToggle = function()
 	{
@@ -15,16 +23,11 @@ fodinhaJS.controller("fodinhaJSctrl", function($scope, $timeout)
 			$scope.loggedIn = false;
 			$timeout( function() {
 				$scope.loggedIn = "yes";
-			}, 500);
+			}, 550);
 
 			//Add new player
 			// TODO USING NODE SERVER
-			$scope.players.push(
-				{
-					name : $scope.nick,
-					ready : $scope.ready
-				}
-			);
+			$scope.players.push($scope.me);
 
 		}
 		// If logging out
@@ -34,42 +37,39 @@ fodinhaJS.controller("fodinhaJSctrl", function($scope, $timeout)
 			$scope.loggedIn = false;
 			$timeout( function() {
 				$scope.loggedIn = "no";
-			}, 500);
+			}, 550);
 
 			//Remove current player
 			// TODO USING NODE SERVER
 			$scope.players = $scope.players.filter(function (player)
 			{
-				return player.name != $scope.nick;
+				return player != $scope.me;
 			});
 		}
 
 		// Reset ready status
-		$scope.ready = false;
+		$scope.me.ready = false;
 	}
 
 	$scope.readyToggle = function()
 	{
-		var i;
-		for(i = 0; i < $scope.players.length; i++)
-		{
-			if($scope.players[i].name == $scope.nick)
-			{
-				console.log("achou");
-				$scope.players[i].ready = !$scope.players[i].ready;
-				break;
-			}
-		}
+		$scope.me.ready = !$scope.me.ready;
 	}
 
 	$scope.players = [
 	{
 		name : "Teste1",
-		ready : false
+		ready : false,
+		lives : 3,
+		won : 2,
+		bet : 0,
 	},
 	{
 		name : "Teste2",
-		ready : false
+		ready : false,
+		lives : 2,
+		won : 2,
+		bet : 0,
 	}
 	];
 });
