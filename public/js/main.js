4a4d9e7f8ddbe1afe827b6d57bb2e7c512adc59c
Jota914 <jpsl@cin.ufpe.br>
2016-04-19 23:16:16 -0300

    Card display in page

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,71 @@
-var fodinhaJS = angular.module ("fodinhaJSapp", ["ngAnimate"]);
+var fodinhaJS = angular.module ("fodinhaJSapp", ["ngAnimate", "ngSanitize"]);
 
-// TODO - CARDS
+fodinhaJS.filter('cardFilter', function($sce)
+{
+	return function(input){
+		var result = "";
+		if(input == "") return "&#x1F0A0"
+
+		switch (input[1]){
+			case 'S':
+				result = "&#x1F0A";
+				break;
+			case 'H':
+				result = "&#x1F0B";
+				break;
+			case 'C':
+				result = "&#x1F0D";
+				break;
+			case 'D':
+				result = "&#x1F0C";
+				break;
+			default:
+				result = "&#1F0A0";
+		}
+
+		switch(input[0]){
+			case '2':
+			case '3':
+			case '4':
+			case '5':
+			case '6':
+			case '7':
+				result += input[0];
+				break;
+			case 'Q':
+				result += "D";
+				break;
+			case 'J':
+				result += "B";
+				break;
+			case 'K':
+				result += "E";
+				break;
+			case 'A':
+				result += "1";
+				break;
+			default:
+				break;
+		}
+
+		return result;
+	};
+});
 
 fodinhaJS.controller("fodinhaJSctrl", function($scope, $timeout)
 {
+	//Local variables
 	$scope.loggedIn = 'no';
 	$scope.me = {
 		name : "",
 		ready : false,
 		lives : 1,
 		won : 2,
-		bet : 0
-
+		bet : 0,
+		card : ''
 	}
 
+	//login button function
 	$scope.loginToggle = function()
 	{
 		// If logging in
@@ -51,11 +103,18 @@ fodinhaJS.controller("fodinhaJSctrl", function($scope, $timeout)
 		$scope.me.ready = false;
 	}
 
+	// Ready button function
 	$scope.readyToggle = function()
 	{
 		$scope.me.ready = !$scope.me.ready;
 	}
 
+	$scope.cardIsRed = function(card)
+	{
+		return (card[1] == 'H' || card[1] == 'D');
+	}
+
+	// Local player list
 	$scope.players = [
 	{
 		name : "Teste1",
@@ -63,6 +122,7 @@ fodinhaJS.controller("fodinhaJSctrl", function($scope, $timeout)
 		lives : 3,
 		won : 2,
 		bet : 0,
+		card : "4C",
 	},
 	{
 		name : "Teste2",
@@ -70,8 +130,14 @@ fodinhaJS.controller("fodinhaJSctrl", function($scope, $timeout)
 		lives : 2,
 		won : 2,
 		bet : 0,
+		card : "7H"
 	}
 	];
+
+	// Local player card list
+	$scope.cards = [
+		"4S","5H","6C", "7D"
+	]
 });
 
 // $(function() {
