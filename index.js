604df1df097acf42c9caac7a9ca21f16a519d62c
Jota914 <jpsl@cin.ufpe.br>
2016-04-26 02:15:00 -0300

    Player login remote/local

--- index.js
+++ index.js
@@ -20,26 +20,71 @@ server.listen(app.get('port'), function () {
   console.log('Server listening at port %d', app.get('port'));
 });
 
-var numUsers = 0;
+//Server Variables
+var players = [];
 
+var convertPlayer = function(player)
+{
+    var clientPlayer = {
+        name: player.name,
+        ready:  player.ready,
+        lives: player.lives,
+        won: player.won,
+        bet: player.bet,
+        card: ""
+    };
+
+    return clientPlayer;
+}
+
+var createClientPlayerList = function()
+{
+    var clientPlayerList = [];
+
+    for(var i = 0; i < players.length; i++)
+    {
+        clientPlayerList.push(convertPlayer(players[i]));
+    }
+
+    return clientPlayerList;
+};
+
+//Server connection code
 io.on('connection', function (socket)
 {
-    var deck = new Deck ();
-    deck.generate();
+    console.log("entrou");
 
-	socket.on('send message', function (data)
+	socket.on('login', function (name, callback)
 	{
-		console.log("teste");
+        //Return playerlist to logged in player
+        callback(createClientPlayerList());
+
+        //Add new player to server player list
+		var newPlayer = new Player (name);
+        players.push(newPlayer);
+
+        //Update new player to other clients
+        socket.broadcast.emit('playerConnect',convertPlayer(newPlayer));
 	});
+
+    socket.on('logout', function(name){
+        //Remove player from server playlist
+        players = players.filter(function (player)
+        {
+            return player.name != name;
+        });
+
+        //Update removed player to other clients
+        socket.broadcast.emit('playerDisconnect', name);
+    });
+
 	socket.on('disconnect', function(){
-		console.log(numUsers + " saiu");
-		numUsers--;
 	});
 });
 
 
 /*
-io.sockets.emit() - send to all connected clients (same as socket.emit)
+io.sockets.emit() - send to all connected clients
 io.sockets.on() - initial connection from a client.
 
 socket.on() - event listener, can be called on client to execute on server
