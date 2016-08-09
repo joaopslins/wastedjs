81f6bd12bc0b53193ad78612255f145de9f7bd69
Jota914 <jpsl@cin.ufpe.br>
2016-08-09 02:33:48 -0300

    Code refactor, for issue #15 [Part 3]
     Topics refactored:
        Factories

--- public/app/socketService.js
+++ public/app/socketService.js
@@ -7,39 +7,53 @@
 	function socket ($rootScope) {
 		var socket = null;
 
-		return {
-			connect: function() {
-				socket = io.connect();
-			},
-			disconnect: function() {
-				socket.disconnect();
-				socket = null;
-			},
-			isConnected: function() {
-				if (socket) {
-					return true;
-				} else {
-					return false;
-				}
-			},
-			on: function (eventName, callback) {
-				socket.on(eventName, function () {
-					var args = arguments;
-					$rootScope.$apply(function () {
-						callback.apply(socket, args);
-					});
-				});
-			},
-			emit: function (eventName, data, callback) {
-				socket.emit(eventName, data, function () {
-					var args = arguments;
-					$rootScope.$apply(function () {
-						if (callback) {
-							callback.apply(socket, args);
-						}
-					});
-				})
+		var socketService = {
+			connect: connect,
+			disconnect: disconnect,
+			isConnected: isConnected,
+			on: on,
+			emit: emit
+		};
+
+		return socketService;
+
+		///////////////////
+
+		function connect() {
+			socket = io.connect();
+		};
+
+		function disconnect() {
+			socket.disconnect();
+			socket = null;
+		};
+
+		function isConnected() {
+			if (socket) {
+				return true;
+			} else {
+				return false;
 			}
+		};
+
+		function on(eventName, callback) {
+			socket.on(eventName, function () {
+				var args = arguments;
+				$rootScope.$apply(function () {
+					callback.apply(socket, args);
+				});
+			});
+		};
+
+		function emit(eventName, data, callback) {
+			socket.emit(eventName, data, function () {
+				var args = arguments;
+				$rootScope.$apply(function () {
+					if (callback) {
+						callback.apply(socket, args);
+					}
+				});
+			})
 		};
 	};
 })();
