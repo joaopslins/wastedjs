32659ae7bdfbe7c64e8811c07b5786ce629a056c
Jota914 <jpsl@cin.ufpe.br>
2016-08-14 17:03:45 -0300

    Separated callbacks into own functions server-side

--- index.js
+++ index.js
@@ -28,99 +28,35 @@ var players = [];
 var game = null;
 var callInterval = 3000; //in ms
 
-var convertPlayer = function(player)
-{
-    var clientPlayer = {
-        name: player.name,
-        ready:  player.ready,
-        lives: player.lives,
-        won: player.won,
-        bet: player.bet,
-        isAlive: player.isAlive,
-        card: ''
-    };
-
-    return clientPlayer;
-};
-
-var createClientPlayerList = function(playerL)
-{
-    var clientPlayerList = [];
-
-    for(var i = 0; i < playerL.length; i++)
-    {
-        clientPlayerList.push(convertPlayer(playerL[i]));
-    }
-
-    return clientPlayerList;
-};
-
-var isNameAvailable = function(name){
-    for(var i in players)
-    {
-        if(players[i].name == name)
-        {
-            return false;
-        }
-    }
-
-    return true;
-};
-
-var disconnect_C = function(socket){
-    //Set name as socket name
-	var name = socket.name;
-
-    //If undefined, dont close game;
-    if (name == null) {
-        return;
-    }
-
-    //Remove player from server playlist
-    players = players.filter(function (player) {
-        return player.name != name;
-    });
-
-    //Undo game
-    if (game !== null) {
-        game = null;
-
-        //Reset players
-        for (var i in players) {
-            var player = players[i];
-
-            player.ready = false;
-            player.lives = 3;
-            player.won = 0;
-            player.bet = 0;
-        }
-    }
-
-    //Reset socket name
-    socket.name = null;
-
-    //Update removed player to other clients
-    socket.broadcast.emit('player-disconnect', name);
-};
-
 //Server connection code
 io.on('connection', function (socket)
 {
-	socket.on('login', function (name, callback) {
+	socket.on('login', loginCB);
+	socket.on('ready', readyCB);
+    socket.on('player-bet',playerBetCB);
+    socket.on('player-play-card', playerPlayCB);
+    socket.on('start-game', startGameCB);
+    socket.on('request-cards', requestCardsCB);
+    socket.on('request-playerlist', requestPlayerlistCB);
+	socket.on('disconnect', disconnectCB);
+
+    //////////////////////////////
+    // Function Implementations //
+    //////////////////////////////
+
+    function loginCB(name, callback) {
         console.log(name + " login");
 
         let success = 0;
 
-        //If name not available
         if (!isNameAvailable(name)) {
+            //If name not available
             success = 1;
-        }
-        //If game is full
-        else if(players.length == 5) {
+        } else if(players.length == 5) {
+            //If game is full
             success = 2;
-        }
-        //If game is on
-        else if (game != null) {
+        } else if (game != null) {
+            //If game is on
             success = 3;
         }
 
@@ -136,26 +72,22 @@ io.on('connection', function (socket)
             //Update new player to other clients
             socket.broadcast.emit('player-connect',convertPlayer(newPlayer));
         }
-	});
+	};
 
-    // Client = {name,ready}
-	socket.on('ready', function(client) {
+    function readyCB(client) {
         console.log(client.name + " ready is " + client.ready);
 		//Update player in server player list
-		for(let i in players)
-		{
-			if (players[i].name == client.name)
-			{
+		for (let i in players) {
+			if (players[i].name == client.name) {
 				players[i].ready = client.ready;
 				socket.broadcast.emit('update-client-ready', client);
 				//Probably unnecessary
 				break;
 			}
 		}
-	});
+	};
 
-    //Player has done a bet
-    socket.on('player-bet', function(bet) {
+    function playerBetCB(bet) {
         console.log(socket.name + " betted " + bet);
         game.playerBet(socket.name, bet);
         let startPlayPhase = game.phase == "play";
@@ -166,10 +98,9 @@ io.on('connection', function (socket)
             "nextPlayer": game.roundPlayer,
             "startPlayPhase": startPlayPhase
         });
-    });
+    };
 
-    //Player played a card
-    socket.on('player-play-card', function(card) {
+    function playerPlayCB(card) {
         console.log(socket.name + " player card " + card);
         game.playerPlayCard(socket.name, card);
 
@@ -226,33 +157,98 @@ io.on('connection', function (socket)
                 "nextPlayer": game.roundPlayer
             });
         }
-    });
+    };
 
-    //Host client started game
-    socket.on('start-game', function() {
+    function startGameCB() {
         console.log("game started!");
         game = new Game (players);
         io.emit('game-start-notification', game.startMatchPlayer);
-    });
+    };
 
-    //Client requested his cards
-    socket.on('request-cards', function(data, callback) {
+    function requestCardsCB(data, callback) {
         console.log(socket.name + " asked his cards");
         callback({
             'cards' : game.getCardsFromPlayer(socket.name)
         });
-    });
+    };
 
-    socket.on('request-playerlist', function(data, callback) {
+    function requestPlayerlistCB(data, callback) {
         console.log(socket.name + " asked for playerlist");
         callback({
             'name' : socket.name,
             'playerList' : createClientPlayerList(players),
         })
-    });
+    };
 
-	socket.on('disconnect', function() {
+    function disconnectCB() {
         console.log(socket.name + " disconnected");
-        disconnect_C(socket);
-    });
+    	let name = socket.name;
+
+        //If undefined, dont close game;
+        if (name == null) {
+            return;
+        }
+
+        //Remove player from server playlist
+        players = players.filter(function (player) {
+            return player.name != name;
+        });
+
+        //Undo game
+        if (game !== null) {
+            game = null;
+
+            //Reset players
+            for (let i in players) {
+                var player = players[i];
+
+                player.ready = false;
+                player.lives = 3;
+                player.won = 0;
+                player.bet = 0;
+            }
+        }
+
+        //Update removed player to other clients
+        socket.broadcast.emit('player-disconnect', name);
+    };
+
+    /////////////////////////////
+    //    Utility Functions    //
+    /////////////////////////////
+
+    function convertPlayer(player) {
+        var clientPlayer = {
+            name: player.name,
+            ready:  player.ready,
+            lives: player.lives,
+            won: player.won,
+            bet: player.bet,
+            isAlive: player.isAlive,
+            card: ''
+        };
+
+        return clientPlayer;
+    };
+
+    function createClientPlayerList(playerL) {
+        var clientPlayerList = [];
+
+        for (var i = 0; i < playerL.length; i++) {
+            clientPlayerList.push(convertPlayer(playerL[i]));
+        }
+
+        return clientPlayerList;
+    };
+
+    function isNameAvailable(name) {
+        for (var i in players) {
+            if (players[i].name == name) {
+                return false;
+            }
+        }
+
+        return true;
+    };
+
 });
