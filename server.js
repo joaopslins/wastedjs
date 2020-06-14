d0e7b79f302a2f41b8d5ad25a2811afd84e7f18c
João Lins <joaopslins@gmail.com>
2020-06-14 16:22:16 -0300

    Adds dirname variable

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import path from "path";
 
 const app = express();
 const port = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
