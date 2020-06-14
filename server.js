f20c36c2e34a0ff6a3a18013120562ef80234097
João Lins <joaopslins@gmail.com>
2020-06-14 17:24:17 -0300

    Setup socketio on server

--- server.js
+++ server.js
@@ -1,24 +1,18 @@
 import express from "express";
 import bodyParser from "body-parser";
 import path from "path";
+import http from "http";
+import socket_io from "socket.io";
+import { init } from "./src/io";
 
 const app = express();
 const port = process.env.PORT || 5000;
 const __dirname = path.resolve();
+const server = http.createServer(app);
+const io = socket_io(server);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get("/api/hello", (req, res) => {
-  res.send({ express: "Hello From Express" });
-});
-
-app.post("/api/world", (req, res) => {
-  console.log(req.body);
-  res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`
-  );
-});
 
 if (process.env.NODE_ENV === "production") {
   // Serve any static files
@@ -30,4 +24,6 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+init(io);
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
